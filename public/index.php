9ed755eb1300a8ece5022296425468449f96eaab
Vitaliy <tsoyvetal@gmail.com>
2025-04-27 22:23:50 +0300

    1st task

--- public/index.php
+++ public/index.php
@@ -1,16 +1,36 @@
 <?php
 
+namespace App;
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use PDO;
+use Symfony\Component\VarDumper\VarDumper;
+use DI\Container;
+use Slim\Middleware\MethodOverrideMiddleware;
+use Illuminate\Support\Collection;
+
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container();
+
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('Hello from Hexlet!');
-    return $response;
-});
+$app->add(MethodOverrideMiddleware::class);
+$app->addErrorMiddleware(true, true, true);
+
+$router = $app->getRouteCollector()->getRouteParser();
+
+
+$app->get('/', function (Request $request, Response $response) use ($router) {
+    return $this->get('renderer')->render($response, 'home.phtml', []);
+})->setName('home');
 
 $app->run();
