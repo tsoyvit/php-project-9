d5e61d6f259d918af2115b687ea6aa0c68ec8cfd
Vitaliy <tsoyvetal@gmail.com>
2025-05-05 23:43:36 +0300

    Refactoring Parser, Fetcher

--- src/Services/Parser.php
+++ src/Services/Parser.php
@@ -12,23 +12,32 @@ class Parser
      * @throws InvalidSelectorException
      */
 
-
     public function parse(string $html): array
     {
         $doc = new Document($html);
 
-        /** @var Element|null $titleElement */
-        /** @var Element|null $h1Element */
-        /** @var Element|null $metaElement */
-
-        $titleElement = $doc->first('title');
-        $h1Element = $doc->first('h1');
-        $metaElement = $doc->first('meta[name=description]');
-
         return [
-            'title' => $titleElement?->text(),
-            'h1' => $h1Element?->text(),
-            'description' => $metaElement?->attr('content')
+            'title' => $this->extractText($doc, 'title'),
+            'h1' => $this->extractText($doc, 'h1'),
+            'description' => $this->extractAttribute($doc, 'meta[name=description]', 'content')
         ];
+    }
+
+    /**
+     * @throws InvalidSelectorException
+     */
+    private function extractText(Document $doc, string $selector): ?string
+    {
+        $element = $doc->first($selector);
+        return $element instanceof Element ? $element->text() : null;
+    }
+
+    /**
+     * @throws InvalidSelectorException
+     */
+    private function extractAttribute(Document $doc, string $selector, string $attr): ?string
+    {
+        $element = $doc->first($selector);
+        return $element instanceof Element ? $element->attr($attr) : null;
     }
 }
