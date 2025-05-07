6583d57d338089e28e167ae1ed40a495aa3e5001
Vitaliy <tsoyvetal@gmail.com>
2025-05-07 14:43:32 +0300

    -redirect

--- src/PageAnalyzer.php
+++ src/PageAnalyzer.php
@@ -19,40 +19,6 @@ class PageAnalyzer
         $this->parse = $parse;
     }
 
-//    public function analyze(string $url): PageCheckResult
-//    {
-//        try {
-//            $response = $this->client->request('GET', $url);
-//            $html = (string)$response->getBody();
-//            $parsed = $this->parse->parse($html);
-//
-//            return new PageCheckResult(
-//                statusCode: $response->getStatusCode(),
-//                h1: $parsed['h1'],
-//                title: $parsed['title'],
-//                description: $parsed['description']
-//            );
-//        } catch (ConnectException $e) {
-//            return new PageCheckResult(error: $e->getMessage());
-//        } catch (RequestException $e) {
-//            $response = $e->getResponse();
-//            if ($response !== null) {
-//                $html = (string)$response->getBody();
-//                $parsed = $this->parse->parse($html);
-//
-//                return new PageCheckResult(
-//                    statusCode: $response->getStatusCode(),
-//                    h1: $parsed['h1'],
-//                    title: $parsed['title'],
-//                    description: $parsed['description'],
-//                    error: $e->getMessage()
-//                );
-//            }
-//            return new PageCheckResult(error: $e->getMessage());
-//        } catch (GuzzleException $e) {
-//            return new PageCheckResult(error: $e->getMessage());
-//        }
-//    }
     public function analyze(string $url): PageCheckResult
     {
         try {
